b8689c0f203011a6ebaff2135e0e7fc1a833ff1f
ParekhAmi <aartipatel@macbook-pro>
2024-06-09 21:20:56 -0400

    First commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author aartipatel
+ *
+ */
+module TestNGProject1 {
+}
---
+++ src/selenium/Testing1.java
@@ -0,0 +1,10 @@
+package selenium;
+
+import org.testng.annotations.Test;
+
+public class Testing1 {
+  @Test
+  public void test1() {
+	  System.out.println("test1");
+  }
+}
